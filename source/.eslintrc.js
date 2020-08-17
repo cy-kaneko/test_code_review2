d63c5192080a5062db6972888899f44b5e6499d5
yoko-kaneko <yoko@cybozu.co.jp>
2020-08-17 14:47:55 +0900

    Update eslintrc

--- source/.eslintrc.js
+++ source/.eslintrc.js
@@ -2,8 +2,7 @@ module.exports = {
     extends: "@cybozu/eslint-config/presets/kintone-customize-es5",
     "env": {"browser": true},
     "globals": {
-        "kintone": true,
-        "kintoneUIComponent": true,
-        "KintoneRestAPIClient": true
+        "kintoneUIComponent": false,
+        "KintoneRestAPIClient": false
     }
 };
